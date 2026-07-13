f3b549f9f01f801f3c4a80b305c4e8a048e4233e
Louis <arogundadelouis22@gmail.com>
2026-07-13 21:00:37 +0100

    Reedited

--- switch.js
+++ switch.js
@@ -2,23 +2,37 @@ const light = document.getElementById("light");
 const darkness = document.getElementById("darkness");
 const bulb = document.getElementById("bulb");
 
-light.addEventListener("click", function(){
-    bulb.src = "Images/on bulb.png"
-    bulb.style.width = "230px"
-    light.style.backgroundColor = "yellow"
+let isBulbOn = false;
 
-    setTimeout(function(){
+light.addEventListener("click", function(){
+
+    if(isBulbOn === false){
+        bulb.src = "Images/on bulb.png"
+        bulb.style.width = "230px"
+        light.style.backgroundColor = "yellow";
+
+        isBulbOn = true;
+
+        setTimeout(function(){
         alert("UP NEPA!");    
     }, 1000);
+    }  
 });
 
 darkness.addEventListener("click", function(){
-    bulb.src = "Images/bulb_PNG1243.png" 
-    bulb.style.width = "250px"
-    darkness.style.backgroundColor = "red"
-    darkness.style.color = "white"
 
-    setTimeout(function(){
-        alert("DOWN NEPA!");
-    }, 1000)
+    if(isBulbOn === true){
+
+        bulb.src = "Images/bulb_PNG1243.png";
+        bulb.style.width = "250px";
+        darkness.style.backgroundColor = "red";
+        darkness.style.color = "white";
+
+        isBulbOn = false;
+
+        setTimeout(function(){
+            alert("DOWN NEPA!");
+        }, 1000)
+    }
+    
 });
